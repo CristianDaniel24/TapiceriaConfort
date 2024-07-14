b98c4dab2993a6db5610d6066dc5a79337b009e7
CristianDaniel24 <cristianponguta789@gmail.com>
2024-07-14 11:47:09 -0500

    Clase Producto

---
+++ entities/Product.java
@@ -0,0 +1,11 @@
+package Proyecto.TapiceriaConfort.entities;
+
+public class Product{
+
+    private String name;
+    private Double price;
+    private int stock;
+
+    public void displayDetails(){
+    }
+}
